5506a2558c8f81b8dd8866eae4ff63a93dea7e97
high-moctane <high.moctane@moctane.com>
2023-09-03 18:13:32 +0900

    Impl Server

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21.0
 
 require (
 	github.com/btcsuite/btcd/btcec/v2 v2.3.2
+	github.com/gobwas/ws v1.3.0
 	github.com/google/uuid v1.3.1
 	github.com/stretchr/testify v1.8.4
 )
@@ -13,6 +14,9 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/decred/dcrd/crypto/blake256 v1.0.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ server.go
@@ -0,0 +1,107 @@
+package mocrelay
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
+	"github.com/high-moctane/mocrelay/nostr"
+)
+
+var ErrRelayStop = errors.New("relay stopped")
+
+type Relay struct {
+	Handler Handler
+}
+
+func (relay *Relay) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// TODO(high-moctane) Use slog
+
+	ctx := r.Context()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	r = r.WithContext(ctx)
+
+	conn, _, _, err := ws.UpgradeHTTP(r, w)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	errs := make(chan error, 2)
+	recv := make(chan nostr.ClientMsg, 1)
+	send := make(chan nostr.ServerMsg, 1)
+
+	go func() {
+		err := relay.serveRead(ctx, conn, recv)
+		errs <- fmt.Errorf("serveRead terminated: %w", err)
+	}()
+
+	go func() {
+		err := relay.serveWrite(ctx, conn, send)
+		errs <- fmt.Errorf("serveWrite terminated: %w", err)
+	}()
+
+	go func() {
+		err := relay.Handler.Handle(r, recv, send)
+		errs <- fmt.Errorf("handle terminated: %w", err)
+	}()
+
+	err = errors.Join(<-errs, <-errs, ErrRelayStop)
+	log.Println(err)
+}
+
+func (relay *Relay) serveRead(ctx context.Context, conn net.Conn, recv chan<- nostr.ClientMsg) error {
+	// TODO(high-moctane) rate-limit
+
+	for {
+		payload, err := wsutil.ReadClientText(conn)
+		if err != nil {
+			return fmt.Errorf("failed to read websocket: %w", err)
+		}
+
+		msg, err := nostr.ParseClientMsg(payload)
+		if err != nil {
+			return fmt.Errorf("failed to parse client msg: %w", err)
+		}
+
+		recv <- msg
+	}
+}
+
+func (relay *Relay) serveWrite(ctx context.Context, conn net.Conn, send <-chan nostr.ServerMsg) error {
+	// TODO(high-moctane) circuit braker
+
+	pingTicker := time.NewTicker(10 * time.Second)
+	defer pingTicker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return fmt.Errorf("serverWrite terminated by ctx: %w", ctx.Err())
+
+		case <-pingTicker.C:
+			if _, err := conn.Write(ws.CompiledPing); err != nil {
+				return fmt.Errorf("failed to send ping: %w", err)
+			}
+
+		case msg := <-send:
+			jsonMsg, err := json.Marshal(msg)
+			if err != nil {
+				return fmt.Errorf("failed to marshal server msg: %w", err)
+			}
+
+			if err := wsutil.WriteServerText(conn, jsonMsg); err != nil {
+				return fmt.Errorf("failed to write websocket: %w", err)
+			}
+		}
+	}
+}
