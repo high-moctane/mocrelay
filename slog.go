3a30eb36a54b77a53a565ed725fc7fbc914728ec
high-moctane <high.moctane@moctane.com>
2023-10-02 23:45:05 +0900

    Added SlogMocrelayHandler

--- slog.go
+++ slog.go
@@ -15,3 +15,35 @@ func mocrelaySlog(ctx context.Context, logger *slog.Logger) *slog.Logger {
 
 	return logger.WithGroup("mocrelay").With(slog.String("RequestID", id), slog.String("ip", ip))
 }
+
+var _ slog.Handler = (*SlogMocrelayHandler)(nil)
+
+type SlogMocrelayHandler struct {
+	h slog.Handler
+}
+
+func WithSlogMocrelayHandler(handler slog.Handler) *SlogMocrelayHandler {
+	return &SlogMocrelayHandler{handler}
+}
+
+func (h *SlogMocrelayHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	return h.h.Enabled(ctx, level)
+}
+
+func (h *SlogMocrelayHandler) Handle(ctx context.Context, record slog.Record) error {
+	id := GetRequestID(ctx)
+	ip := GetRealIP(ctx)
+	return h.h.WithAttrs([]slog.Attr{slog.String("requestID", id), slog.String("realIP", ip)}).
+		Handle(ctx, record)
+}
+
+func (h *SlogMocrelayHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return WithSlogMocrelayHandler(h.h.WithAttrs(attrs))
+}
+
+func (h *SlogMocrelayHandler) WithGroup(name string) slog.Handler {
+	if name == "" {
+		return h
+	}
+	return WithSlogMocrelayHandler(h.h.WithGroup(name))
+}
