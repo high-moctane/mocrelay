160a9db1315a36232421bfb137745d37ca8dfe19
high-moctane <high.moctane@moctane.com>
2023-10-04 21:01:18 +0900

    Impl simple relay server

--- cmd/mocrelay/main.go
+++ cmd/mocrelay/main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log/slog"
+	"net/http"
+	"time"
 
 	"github.com/high-moctane/mocrelay"
 )
 
 func main() {
-	fmt.Println(mocrelay.Mocrelay())
+	ctx := context.Background()
+	h := mocrelay.NewMergeHandler(mocrelay.NewCacheHandler(ctx, 100), mocrelay.NewRouter(nil))
+	h = mocrelay.NewRecvEventUniquefyMiddleware(100)(h)
+	h = mocrelay.NewSendEventUniquefyMiddleware(100)(h)
+	h = mocrelay.NewEventCreatedAtReqFilterMiddleware(-5*time.Minute, 1*time.Minute)(h)
+
+	r := mocrelay.NewRelay(h, &mocrelay.RelayOption{
+		Logger:     slog.Default(),
+		RecvLogger: slog.Default(),
+		SendLogger: slog.Default(),
+	})
+
+	slog.ErrorContext(ctx, "mocrelay terminated", http.ListenAndServe("localhost:8234", r))
 }
