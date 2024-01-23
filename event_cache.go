88601be8fec796ebc2b64f136c14dc33c698b8fe
high-moctane <high.moctane@moctane.com>
2024-01-24 01:48:18 +0900

    Use sync.Pool in eventCacheEvsIndex.Find()

--- event_cache.go
+++ event_cache.go
@@ -269,14 +269,20 @@ const (
 )
 
 type eventCacheEvsIndex struct {
-	total int
-	idx   map[eventCacheEvsIndexKey]map[*Event]bool
+	total       int
+	idx         map[eventCacheEvsIndexKey]map[*Event]bool
+	findMapPool sync.Pool
 }
 
 func newEventCacheEvsIndex(total int) *eventCacheEvsIndex {
 	return &eventCacheEvsIndex{
 		total: total,
 		idx:   make(map[eventCacheEvsIndexKey]map[*Event]bool),
+		findMapPool: sync.Pool{
+			New: func() any {
+				return make(map[*Event]bool, total)
+			},
+		},
 	}
 }
 
@@ -395,12 +401,11 @@ func (c *eventCacheEvsIndex) Find(
 	idMaps := make([]map[*Event]bool, 0, len(keysSlice))
 
 	for _, keys := range keysSlice {
-		size := 0
-		for _, key := range keys {
-			size += len(c.idx[key])
+		m := c.findMapPool.Get().(map[*Event]bool)
+		for ev := range m {
+			delete(m, ev)
 		}
 
-		m := make(map[*Event]bool, min(size, c.total))
 		for _, key := range keys {
 			for ev := range c.idx[key] {
 				m[ev] = true
@@ -423,6 +428,7 @@ func (c *eventCacheEvsIndex) Find(
 			}
 		}
 
+		c.findMapPool.Put(mlast)
 		idMaps = idMaps[:len(idMaps)-1]
 	}
 
@@ -448,6 +454,8 @@ func (c *eventCacheEvsIndex) Find(
 			cnt--
 		}
 	}
+
+	c.findMapPool.Put(idMaps[0])
 
 	return
 }
