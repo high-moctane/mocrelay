9650336c92e6f48637e7e066b11d4d5f22042300
high-moctane <high.moctane@moctane.com>
2024-01-23 22:43:48 +0900

    Use mutexes in NewSimpleMiddleware()

--- handler.go
+++ handler.go
@@ -9,6 +9,7 @@ import (
 	"io"
 	"slices"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/google/uuid"
@@ -873,6 +874,8 @@ func NewSimpleMiddleware(bases ...SimpleMiddlewareBaseInterface) SimpleMiddlewar
 					}
 				}
 
+				mus := make([]sync.Mutex, len(bases))
+
 				ctx, cancel := context.WithCancel(ctx)
 				defer cancel()
 
@@ -886,13 +889,13 @@ func NewSimpleMiddleware(bases ...SimpleMiddlewareBaseInterface) SimpleMiddlewar
 					defer cancel()
 					defer close(rCh)
 
-					errs <- simpleMiddlewareHandleRecv(ctx, bases, recv, send, rCh)
+					errs <- simpleMiddlewareHandleRecv(ctx, mus, bases, recv, send, rCh)
 				}()
 
 				go func() {
 					defer cancel()
 
-					errs <- simpleMiddlewareHandleSend(ctx, bases, send, sCh)
+					errs <- simpleMiddlewareHandleSend(ctx, mus, bases, send, sCh)
 				}()
 
 				defer cancel()
@@ -904,6 +907,7 @@ func NewSimpleMiddleware(bases ...SimpleMiddlewareBaseInterface) SimpleMiddlewar
 
 func simpleMiddlewareHandleRecv(
 	ctx context.Context,
+	mus []sync.Mutex,
 	bases []SimpleMiddlewareBaseInterface,
 	recv <-chan ClientMsg,
 	send chan<- ServerMsg,
@@ -921,10 +925,13 @@ func simpleMiddlewareHandleRecv(
 
 			cmsgs := []ClientMsg{cmsg}
 
-			for _, base := range bases {
+			for i, base := range bases {
 				var nextCmsgs []ClientMsg
 				for _, cmsg := range cmsgs {
+					mus[i].Lock()
 					cmsgCh, smsgCh, err := base.HandleClientMsg(ctx, cmsg)
+					mus[i].Unlock()
+
 					if err != nil {
 						return err
 					}
@@ -971,6 +978,7 @@ func simpleMiddlewareHandleRecv(
 
 func simpleMiddlewareHandleSend(
 	ctx context.Context,
+	mus []sync.Mutex,
 	bases []SimpleMiddlewareBaseInterface,
 	send chan<- ServerMsg,
 	sCh chan ServerMsg,
@@ -985,7 +993,10 @@ func simpleMiddlewareHandleSend(
 			for i := len(bases) - 1; 0 <= i; i-- {
 				var nextSmsgs []ServerMsg
 				for _, smsg := range smsgs {
+					mus[i].Lock()
 					smsgCh, err := bases[i].HandleServerMsg(ctx, smsg)
+					mus[i].Unlock()
+
 					if err != nil {
 						return err
 					}
