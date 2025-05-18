4762b939459fb0c006d2009b7713d2e559014473
high-moctane <high.moctane@moctane.net>
2025-05-18 22:47:43 +0900

    neo_handler: NewNeoSimpleHandler()

--- neo_handler.go
+++ neo_handler.go
@@ -1,6 +1,8 @@
 package mocrelay
 
-import "context"
+import (
+	"context"
+)
 
 type NeoHandler interface {
 	NeoServeNostr(ctx context.Context, send chan<- ServerMsg, recv <-chan ClientMsg) error
@@ -20,4 +22,54 @@ type NeoSimpleHandlerBase interface {
 	NeoServeNostrOnStart(ctx context.Context) (newCtx context.Context, err error)
 	NeoServeNostrClientMsg(ctx context.Context, msg ClientMsg) (<-chan ServerMsg, error)
 	NeoServeNostrOnEnd(ctx context.Context, serveErr error) error
+}
+
+type NeoSimpleHandler NeoHandlerFunc
+
+func NewNeoSimpleHandler(base NeoSimpleHandlerBase) NeoHandler {
+	return NeoHandlerFunc(func(ctx context.Context, send chan<- ServerMsg, recv <-chan ClientMsg) (err error) {
+		defer func() {
+			err = base.NeoServeNostrOnEnd(ctx, err)
+		}()
+
+		ctx, err = base.NeoServeNostrOnStart(ctx)
+		if err != nil {
+			return
+		}
+
+		err = func() error {
+			for cmsg, err := range recvCtx(ctx, recv) {
+				if err != nil {
+					return err
+				}
+
+				smsgCh, err := base.NeoServeNostrClientMsg(ctx, cmsg)
+				if err != nil {
+					return err
+				}
+				if smsgCh == nil {
+					continue
+				}
+
+				for smsg, err := range recvCtx(ctx, smsgCh) {
+					if err != nil {
+						if err == errRecvCtxChanClosed {
+							break
+						}
+						return err
+					}
+					if err := sendCtx(ctx, send, smsg); err != nil {
+						return err
+					}
+				}
+			}
+
+			panic("unreachable")
+		}()
+
+		if err == errRecvCtxChanClosed {
+			err = ErrRecvClosed
+		}
+		return
+	})
 }
