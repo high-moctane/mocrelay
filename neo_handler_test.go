e37b937d638e6e10120396d9155f35074a018b34
high-moctane <high.moctane@moctane.net>
2025-05-20 09:20:22 +0900

    neo_handler_test: add test

--- neo_handler_test.go
+++ neo_handler_test.go
@@ -65,4 +65,60 @@ func TestNeoSimpleHandler(t *testing.T) {
 		default:
 		}
 	})
+
+	t.Run("context passing", func(t *testing.T) {
+		ctxKey := "testKey"
+
+		want := "testValue"
+		var got string
+
+		base := &mockNeoSimpleHandlerBase{
+			neoServeNostrOnStartFunc: func(ctx context.Context) (context.Context, error) {
+				return context.WithValue(ctx, ctxKey, "testValue"), nil
+			},
+			neoServeNostrClientMsg: func(ctx context.Context, msg ClientMsg) (<-chan ServerMsg, error) {
+				return newClosedBufCh[ServerMsg](&ServerClosedMsg{}), nil
+			},
+			neoServeNostrOnEnd: func(ctx context.Context, err error) error {
+				var ok bool
+				got, ok = ctx.Value(ctxKey).(string)
+				if !ok {
+					t.Fatal("expected context to contain key")
+				}
+				return nil
+			},
+		}
+
+		send := make(chan ServerMsg, 10)
+		recv := newClosedBufCh[ClientMsg](&ClientReqMsg{})
+
+		ctx := t.Context()
+
+		handler := NewNeoSimpleHandler(base)
+
+		err := handler.NeoServeNostr(ctx, send, recv)
+		if err != nil {
+			t.Errorf("expected no error, got %v", err)
+		}
+
+		select {
+		case msg := <-send:
+			if _, ok := msg.(*ServerClosedMsg); !ok {
+				t.Errorf("expected message to be of type ServerClosedMsg, got %T", msg)
+			}
+		default:
+			t.Error("expected message to be sent, but none was sent")
+		}
+
+		select {
+		case msg := <-send:
+			t.Errorf("got extra message: %v", msg)
+
+		default:
+		}
+
+		if got != want {
+			t.Errorf("expected context value to be %v, got %v", want, got)
+		}
+	})
 }
